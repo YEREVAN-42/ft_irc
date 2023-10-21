2f0f053e2a3cf8228c4fbbd78edb77eee320cd13
Larisa Galstyan <lgalstya@c1r1s3.42yerevan.am>
2023-10-21 17:42:21 +0400

    only template is written.

---
+++ src/include/Channel.hpp
@@ -0,0 +1,15 @@
+#ifndef __CHANNEL_HPP__
+#define __CHANNEL_HPP__
+
+namespace irc
+{
+    class Channel
+    {
+        public:
+
+        private:
+            
+    }; 
+}
+
+#endif // __CHANNEL_HPP__
