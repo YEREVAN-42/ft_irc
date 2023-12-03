5babb025a0d294250360b90ae4425c14e65b8c08
lgalstya <larisagalstya@gmail.com>
2023-12-03 19:12:47 +0400

    Added Mode, but dont compile

--- include/network/Channel.hpp
+++ include/network/Channel.hpp
@@ -1,7 +1,8 @@
-#ifndef __CHANNEL_HPP__
-# define __CHANNEL_HPP__
+#ifndef __Channel_hpp__
+# define __Channel_hpp__
 
 # include "irc.hpp"
+
 
 # include "User.hpp"
 
@@ -11,8 +12,17 @@ namespace irc
 
     class Channel
     {
-        public:
-            typedef std::vector<User *>::iterator userIter;
+       public:
+          enum ChannelMode_t
+		    	{
+		    		INV_ONLY	    	= 1,
+		    		REST_TOPIC    	= 2, //DONE
+		    		PRIVATE_KEY	  	= 4, //DONE
+		    		OPER_PRIVILEGE	= 8,
+            USER_LIMIT      = 16
+		    	};
+
+           typedef std::vector<User *>::const_iterator userIter;
 
         public:
             Channel(const std::string &name, const std::string &key, User* admin);
@@ -22,26 +32,39 @@ namespace irc
             void    kick(User*, User*, const std::string&);
             void    broadcast(const std::string&);
             void    broadcast(const std::string&, const std::string&);
+            void    invite(User*);
+            void    mode(ChannelMode_t); // use mode(inv_only | rest_topic)
 
             void    setKey(const std::string&);
             void    setLimit(size_t);
             void    setExtMsg(bool);
+            void    setModeString(const std::string&);
+            void    setTopic(const std::string&);
 
             const std::string&          getName() const;
             User*                       getAdmin() const;
-            const std::string&          getKey() const;
             size_t                      getLimit() const;
             bool                        getExtMsg() const;
             size_t                      getSize() const;
-            std::vector<std::string>    getNickNames();
+            std::vector<std::string>    getNickNames() const;
+            int                         getMode() const;
+
+            void removeTopic(const std::string&);
+            void removeKey();
+
+        private:
+            void setMode(ChannelMode_t);
+            bool checkMode(ChannelMode_t);
 
         private:
             std::string         _name;
             std::string         _key;
+            std::string         _topic;
             User*               _admin;
             std::vector<User *> _userVector;
             size_t              _limitMember;
             bool                _flag;       // yes/no for external messages
+            int                 _modes;
     }; 
 }
 
