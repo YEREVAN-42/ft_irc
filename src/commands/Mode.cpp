c02b1c1494df20f669e3893ba53b65a80bccebd9
Khachik <93828788+khovakim@users.noreply.github.com>
2023-12-17 18:43:08 +0400

    Update Mode.cpp

--- src/commands/Mode.cpp
+++ src/commands/Mode.cpp
@@ -6,48 +6,26 @@ irc::Mode::~Mode(){}
 void	irc::Mode::execute(User* user, const std::vector<std::string>& args)
 {
 	if (args.empty())
-    {
-        user->reply(ERR_NEEDMOREPARAMS(user->getNickName(), "MODE"));
-        return;
-    }
-
-	std::string	channel_name = args[0];
-
-	Channel* channel = _server->getChannel(channel_name);
-
-	if (!channel)
-    {
-        user->reply(ERR_NOSUCHCHANNEL(user->getNickName(), channel_name));
-        return;
-    }
-
-	if (channel->getAdmin() != user)
-    {
-        user->reply(ERR_CHANOPRIVSNEEDED(user->getNickName(), channel_name));
-        return;
-    }
-
-	if (args.empty())
-    {
-        user->reply(ERR_NEEDMOREPARAMS(user->getNickName(), "MODE"));
-        return;
-    }
+	{
+		user->reply(ERR_NEEDMOREPARAMS(user->getNickName(), "MODE"));
+		return;
+	}
 
 	std::string	channel_name = args[1];
 
 	Channel* channel = _server->getChannel(channel_name);
 
 	if (!channel)
-    {
-        user->reply(ERR_NOSUCHCHANNEL(user->getNickName(), channel_name));
-        return;
-    }
+	{
+        	user->reply(ERR_NOSUCHCHANNEL(user->getNickName(), channel_name));
+        	return;
+	}
 
 	if (channel->getAdmin() != user)
-    {
-        user->reply(ERR_CHANOPRIVSNEEDED(user->getNickName(), channel_name));
-        return;
-    }
+	{
+        	user->reply(ERR_CHANOPRIVSNEEDED(user->getNickName(), channel_name));
+        	return;
+	}
 
 	// std::size_t found = args[2].find_first_not_of("itkol ");
 
